3f8992616e32c7e07547abf291849fc2805143ec
henrydog1107 <91250457+henrydog1107@users.noreply.github.com>
2021-10-28 17:43:42 +0800

    Add files via upload

---
+++ week07/week07-5.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++){
+        printf("筿福癹伴程糶猭:%d\n",i);
+    }
+    for(int i=1; i<=3; i++){
+        printf("摸计计,眖1计:%d\n",i);
+    }
+}
